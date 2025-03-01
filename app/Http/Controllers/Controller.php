d6ff5494c4351af2c76944420b8e5ba6873d0e2d
Victor Manuel Diaz Vigo <victoddr1969@gmail.com>
2025-03-01 09:29:30 -0500

    feat (user): add JSON response functions and configuration for api token usage

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+
 abstract class Controller
 {
     //
+    public static function success(string $message, array $data = [], int $status = 200): JsonResponse
+    {
+        return response()->json(
+            [
+                'status' => true,
+                'message' => $message,
+                'data' => $data
+            ],
+            $status
+        );
+    }
+
+    public static function error(string $message, array $data = [], int $status = 400): JsonResponse
+    {
+        return response()->json(
+            [
+                'status' => false,
+                'message' => $message,
+                'error' => $data
+            ],
+            $status
+        );
+    }
 }
