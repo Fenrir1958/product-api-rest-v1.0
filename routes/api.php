b631ae897a1f6383580d4447c839548b33001a58
Victor Manuel Diaz Vigo <victoddr1969@gmail.com>
2025-03-01 09:54:34 -0500

    feat (auth): define api route for login function

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthenticationController::class, 'login']);
 
 Route::controller(UserController::class)->group(function () {
     Route::post('/createUser', 'store');
