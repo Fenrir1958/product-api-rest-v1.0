ce29b279eb4ecf3f1b7475d4e1e3a9694987e0bf
Victor Manuel Diaz Vigo <victoddr1969@gmail.com>
2025-03-01 10:11:54 -0500

    feat (categories): define routes for categories functions

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\CategorieController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,4 +11,12 @@ Route::post('/login', [AuthenticationController::class, 'login']);
 Route::controller(UserController::class)->group(function () {
     Route::post('/createUser', 'store');
     Route::put('/updateUser/{id}', 'update')->middleware('auth:sanctum');
+});
+
+Route::middleware('auth:sanctum')->controller(CategorieController::class)->group(function () {
+    Route::get('/categories', 'index');
+    Route::post('/createCategorie', 'store');
+    Route::get('/showCategorie/{id}', 'show');
+    Route::put('/updateCategorie/{id}', 'update');
+    Route::delete('/deleteCategorie/{id}', 'destroy');
 });
