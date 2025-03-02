b1bbd21679178eedd1f4c22c7d7833f755d56d43
Victor Manuel Diaz Vigo <victoddr1969@gmail.com>
2025-03-01 21:19:22 -0500

    refactoring: reorganize the seeder code to sort the data load

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,19 +14,15 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-        $this->call(
-            [
-                ProductsSeeder::class,
-            ]
-        );
 
         $this->call(
+            ProductsSeeder::class,
             CategoriesSeeder::class
         );
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin',
+            'email' => 'test@admin.com',
         ]);
     }
 }
